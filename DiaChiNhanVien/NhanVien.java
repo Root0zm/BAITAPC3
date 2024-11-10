cd2e6ad67a7a5f8c8b680548a7c9c87847339c11
Hoang <phamhoang15032005ii@gmail.com>
2024-11-10 12:42:27 +0700

    Update NhanVien.java

--- DiaChiNhanVien/NhanVien.java
+++ DiaChiNhanVien/NhanVien.java
@@ -1,1 +1,50 @@
+package diachinhanvien;
 
+public class NhanVien {
+//thuoc tinh
+	private String name;
+	private DiaChi address;
+	private String dob;
+	private char gender;
+	
+	//contructors 
+	public NhanVien() {
+	}
+	public NhanVien(String name, DiaChi address, String dob, char gender) {
+		this.name = name;
+		this.address = address;
+		this.dob = dob;
+		this.gender = gender;
+	}
+	// getter
+	public String getName() {
+		return name;
+	}
+	public DiaChi getAddress() {
+		return address;
+	}
+	public String getDob() {
+		return dob;
+	}
+	public char getGender() {
+		return gender;
+	}
+	//setter
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setAddress(DiaChi address) {
+		this.address = address;
+	}
+	public void setDob(String dob) {
+		this.dob = dob;
+	}
+	public void setGender(char gender) {
+		this.gender = gender;
+	}
+	@Override
+	public String toString() {
+		return "NhanVien [name=" + name + ", address=" + address + ", dob=" + dob + ", gender=" + gender + "]";
+	}
+	
+}
